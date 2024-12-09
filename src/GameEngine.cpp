b40d8a95292d5b6743d8248e5656aa5ea5b059fa
jskopac <jenniisomduvet@gmail.com>
2024-12-09 14:29:04 +0100

    Added tick

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,6 +1,7 @@
 #include "GameEngine.h"
 #include "Sprite.h"
 #include "Init.h"
+#define FPS 60 
 
 namespace game
 {
@@ -12,12 +13,16 @@ namespace game
 
     void GameEngine ::run()
     {
+        const int tickInterval = 1000 / FPS;
+        Uint32 nextTick;
+        int delay;
         bool quit = false;
         while (!quit)
         {
             SDL_Event event;
             while (SDL_PollEvent(&event))
             {
+                nextTick = SDL_GetTicks() + tickInterval;
                 switch (event.type)
                 {
                 case SDL_QUIT:
@@ -35,6 +40,10 @@ namespace game
                         //metod som kallar action och skickar med event
                     }
                     
+                }
+                delay = nextTick - SDL_GetTicks();
+                if (delay > 0){
+                    SDL_Delay(delay);
                 }
             }
         }
